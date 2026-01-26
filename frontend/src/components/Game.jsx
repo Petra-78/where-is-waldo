45e2661207df2f2b31917ed0c274eec584d6a76a
Petra-78 <partos.petra.0305@gmail.com>
2026-01-26 19:32:57 +0100

    Add game component

---
+++ frontend/src/components/Game.jsx
@@ -0,0 +1,11 @@
+import image from "../../public/where-is-waldo.jpeg";
+
+export default function Game() {
+  return (
+    <>
+      <div>
+        <img src={image} alt="where is waldo" />
+      </div>
+    </>
+  );
+}
