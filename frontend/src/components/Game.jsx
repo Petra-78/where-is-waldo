3d0c25ac6db3baee13e752be1b60e6a24a521c09
Petra-78 <partos.petra.0305@gmail.com>
2026-01-26 19:53:54 +0100

    Add coordinate state + click logic

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -1,11 +1,29 @@
-import image from "../../public/where-is-waldo.jpeg";
+import { useRef, useState } from "react";
+import image from "/where-is-waldo.jpeg";
 
 export default function Game() {
+  const [coordinates, setCoordinates] = useState([]);
+  const imageRef = useRef(null);
+
+  function handleImageClick(e) {
+    const rect = imageRef.current.getBoundingClientRect();
+
+    const x = (e.clientX - rect.left) / rect.width;
+    const y = (e.clientY - rect.top) / rect.height;
+
+    console.log("Normalized coords:", x, y);
+    setCoordinates([x, y]);
+  }
+
   return (
-    <>
-      <div>
-        <img src={image} alt="where is waldo" />
-      </div>
-    </>
+    <div>
+      <img
+        ref={imageRef}
+        src={image}
+        alt="Where is Waldo"
+        onClick={handleImageClick}
+        style={{ width: "100%", maxWidth: "900px", cursor: "crosshair" }}
+      />
+    </div>
   );
 }
