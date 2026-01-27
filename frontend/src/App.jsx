ecb56b2324e02070fd7a481e17d0cfaec67eec39
Petra-78 <partos.petra.0305@gmail.com>
2026-01-27 18:55:30 +0100

    Add toast container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import "./App.css";
 import { Outlet } from "react-router";
+import { ToastContainer, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Outlet />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"
+        transition={Slide}
+      />
     </>
   );
 }
