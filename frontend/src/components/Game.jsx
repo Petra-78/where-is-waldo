8b52ec33d0c6f07d2659b58d66ec9c04a44fa99d
Petra-78 <partos.petra.0305@gmail.com>
2026-01-27 14:19:21 +0100

    Add target box display logic

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -1,8 +1,10 @@
 import { useRef, useState } from "react";
 import image from "/where-is-waldo.jpeg";
+import Target from "./Target";
 
 export default function Game() {
-  const [coordinates, setCoordinates] = useState([]);
+  const [coordinates, setCoordinates] = useState(null);
+  const [showTargetBox, setShowTargetBox] = useState(false);
   const imageRef = useRef(null);
 
   function handleImageClick(e) {
@@ -12,18 +14,40 @@ export default function Game() {
     const y = (e.clientY - rect.top) / rect.height;
 
     console.log("Normalized coords:", x, y);
-    setCoordinates([x, y]);
+    setCoordinates({ x, y });
+    setShowTargetBox(!showTargetBox);
   }
 
   return (
-    <div>
-      <img
-        ref={imageRef}
-        src={image}
-        alt="Where is Waldo"
-        onClick={handleImageClick}
-        style={{ width: "100%", maxWidth: "900px", cursor: "crosshair" }}
-      />
+    <div
+      className="main"
+      onClick={() => {
+        setShowTargetBox(!showTargetBox);
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          display: "inline-block",
+          width: "90%",
+        }}
+      >
+        <img
+          ref={imageRef}
+          src={image}
+          alt="Where is Waldo"
+          onClick={handleImageClick}
+          style={{
+            width: "100%",
+            display: "block",
+            cursor: "crosshair",
+            zIndex: "999",
+          }}
+        />
+        {showTargetBox && coordinates && (
+          <Target x={coordinates.x} y={coordinates.y} />
+        )}
+      </div>
     </div>
   );
 }
