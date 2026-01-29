5c22906a3719c5750b5ac0b1022a541d8212fb6f
Petra-78 <partos.petra.0305@gmail.com>
2026-01-29 17:17:27 +0100

    Add game context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,24 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
 import routes from "./routes";
+import { GameContext } from "./context/gameContext";
 
 const router = createBrowserRouter(routes);
 
+function Root() {
+  const [gameId, setGameId] = useState(null);
+
+  return (
+    <GameContext.Provider value={{ gameId, setGameId }}>
+      <RouterProvider router={router} />
+    </GameContext.Provider>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Root />
   </StrictMode>,
 );
