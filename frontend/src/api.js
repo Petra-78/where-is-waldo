70ee7799af255a8f0f1003493754a29832a04c25
Petra-78 <partos.petra.0305@gmail.com>
2026-01-29 18:34:47 +0100

    Add character fetcher + error handling

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,16 +2,36 @@ export async function startGame() {
   const res = await fetch(`http://localhost:3000/game`, {
     method: "POST",
   });
+
+  if (!res.ok) {
+    throw new Error("Failed to start game");
+  }
+
   const data = await res.json();
+
   return data.gameId;
 }
 
 export async function validateCharacter(gameId, character, x, y) {
-  debugger;
   const res = await fetch(`http://localhost:3000/validate`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ gameId, character, x, y }),
   });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch characters");
+  }
+
   return await res.json();
+}
+
+export async function fetchCharacters() {
+  const res = await fetch("http://localhost:3000/characters");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch characters");
+  }
+
+  return res.json();
 }
