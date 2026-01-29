79ce2a35ab8ebfdf9ea724a7899752e132e74fc8
Petra-78 <partos.petra.0305@gmail.com>
2026-01-29 17:20:01 +0100

    Add game id initiation

--- frontend/src/components/Game.jsx
+++ frontend/src/components/Game.jsx
@@ -1,11 +1,23 @@
 import { useRef, useState, useEffect } from "react";
 import image from "/where-is-waldo.jpeg";
 import Target from "./Target";
+import { useContext } from "react";
+import { GameContext } from "../context/gameContext";
+import { startGame } from "../api";
 
 export default function Game() {
   const [coordinates, setCoordinates] = useState(null);
   const [showTargetBox, setShowTargetBox] = useState(null);
   const imageRef = useRef(null);
+  const { gameId, setGameId } = useContext(GameContext);
+  useEffect(() => {
+    async function initGame() {
+      const id = await startGame();
+      setGameId(id);
+    }
+
+    initGame();
+  }, []);
 
   async function handleImageClick(e) {
     const rect = imageRef.current.getBoundingClientRect();
