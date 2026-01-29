8b10a67a3b084728d69ae5a2ebcfd452e1ad4928
Petra-78 <partos.petra.0305@gmail.com>
2026-01-29 21:48:37 +0100

    Connect score post with backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -35,3 +35,24 @@ export async function fetchCharacters() {
 
   return res.json();
 }
+
+export async function addScore(gameId, name, time) {
+  const res = await fetch(`http://localhost:3000/scores`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ gameId, name, time }),
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch characters");
+  }
+
+  return res.json();
+}
+
+export async function fetchLeaderboard() {
+  debugger;
+  const res = await fetch("http://localhost:3000/scores");
+  if (!res.ok) throw new Error("Failed to fetch leaderboard");
+  return res.json();
+}
