feb13a8412eb6b2419eee0666abea9f075325d21
Petra-78 <partos.petra.0305@gmail.com>
2026-01-30 14:02:26 +0100

    Add homepage css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router";
+import Navbar from "../components/navbar/Navbar";
+import "./Home.css";
 
 export default function Home() {
   return (
     <>
-      <Link to={"/game"}>
-        <button>Start game</button>
-      </Link>
+      <Navbar />
+      <div className="home-background">
+        <div className="home-container">
+          <img src="/where-is-waldo.jpeg" alt="where-is-everyone" />
+          <h1>City of Animals</h1>
+          <Link to="/game">
+            <button className="start-game-btn">Start Game</button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
