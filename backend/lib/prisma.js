67f1d12ce6a1090e6f619938a39b6137ef1c4491
Petra-78 <partos.petra.0305@gmail.com>
2026-01-30 14:59:04 +0100

    Remove adapter-pg

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -1,11 +1,6 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import pkg from "@prisma/client";
 const { PrismaClient } = pkg;
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export { prisma };
